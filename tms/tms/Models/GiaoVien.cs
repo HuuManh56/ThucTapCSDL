32f013783362042080729e78c89d3273b875a026
xuansonkrt <xuansonkrt@gmail.com>
2019-05-13 18:27:24 +0700

    sonnx

---
+++ tms/tms/Models/GiaoVien.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tms.Models
+{
+    public class GiaoVien
+    {
+        public System.Int32 ID
+        {
+            get;
+            set;
+        }
+        public System.Int32 IDBOMON
+        {
+            get;
+            set;
+        }
+        public System.String HOTEN
+        {
+            get;
+            set;
+        }
+        public System.String NGAYSINH
+        {
+            get;
+            set;
+        }
+        public System.String SDT
+        {
+            get;
+            set;
+        }
+        public System.Int32 GIOITINH
+        {
+            get;
+            set;
+        }
+        public System.String EMAIL
+        {
+            get;
+            set;
+        }
+        public System.String DIACHI
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ tms/tms/Models/HocHam.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tms.Models
+{
+    public class HocHam
+    {
+        public System.Int32 ID
+        {
+            get;
+            set;
+        }
+        public System.String TENHOCHAM
+        {
+            get;
+            set;
+        }
+    }
+}
